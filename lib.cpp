e29b99b5158ac14113a6f584f546e6f0526e2a2e
matteoalciati <91663997+matteoalciati@users.noreply.github.com>
2021-12-09 21:38:22 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,20 @@
+#include "lib.h"
+
+
+float funzione(float numeri[],int n){
+    int i=0;
+    float f=0;
+    float k=0;
+    float e=0;
+
+    while(i < n){
+        k=f;
+        numeri[i]=f;
+        i++;
+        if(f>k){
+            e=f;
+        }
+    }
+    return e;
+}
 
