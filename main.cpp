74ecb337ecc1ea690e1d699f94708bd65e5a31c8
matteoalciati <91663997+matteoalciati@users.noreply.github.com>
2021-12-09 21:40:32 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,27 @@
 #include <iostream>
 
-int main(){
-  return 0;
+using namespace std;
+
+
+int main() {
+    float d=0;
+    int i=0;
+    float b=0;
+    int c=0;
+    int n=0;
+    int h=0;
+    float numeromax=0;
+    cin >> n;
+
+    float numeri [n];
+   
+  while (i<n){
+        c=b;
+        cin >> b;
+        numeri[i]=b;
+        i++;
+    }
+  funzione(numeri, n);
+cout << funzione ;
+    return 0;
 }
